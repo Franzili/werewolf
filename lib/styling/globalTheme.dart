86bbb6d8550fa88d5a3e1dab35addf07a3c63b65
fniemeyer <fniemeyer@techfak.uni-bielefeld.de>
2020-07-21 11:11:34 +0200

    Update gitignore

---
+++ lib/styling/globalTheme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+final ThemeData theme = ThemeData(
+    primaryColor: Colors.blue,
+    accentColor: Colors.green,
+    textTheme: TextTheme(bodyText2: TextStyle(color: Colors.purple)),
+);
